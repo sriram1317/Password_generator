a1e126fc387f26e4c53054183fc933384083fe03
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-15 20:09:46 +0530

    Added comments and removed un-needed (for now) variables

--- main.py
+++ main.py
@@ -2,30 +2,34 @@
 
 
 import random
+
+
 print("Welcome to password generator!")
 
+#Getting values
 passLen = int(input("How many letters would you like in your password?\n"))
 passSym = int(input("How many symbols would you like?\n"))
 passNum = int(input("How many num would you like?\n"))
 
 totLen = passLen + passSym + passNum
 
+#the lists from here we are going to take the charecters for the password
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 num = ['1','2','3','4','5','6','7','8','9','0']
 sym = ['!','@','#','$','%','^','&','*','(',')','-','=','+','*','/','[',']','{','}',':',';',',','.','?','<','>']
 
-letLen = len(letters)
-numLen = len(num)
-symLen = len(sym)
-
+# list to store the generated charecters
 pass_gen = []
 
+# get random letters from the list 
 for i in range(0, passLen):
     pass_gen.append(random.choice(letters))
 
+# get random number from the list
 for i in range(0, passNum):
     pass_gen.append(random.choice(num))
 
+# get random symbols from the list
 for i in range(0, passSym):
     pass_gen.append(random.choice(sym))
 
